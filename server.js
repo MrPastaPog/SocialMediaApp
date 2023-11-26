1f6ccf07eb89e9d4727c8ca397057af2c6dddc06
Matteo Sette <m7gsette@gmail.com>
2023-11-26 19:55:18 +1100

    Menu and order

--- server.js
+++ server.js
@@ -2,21 +2,23 @@ const express = require('express')
 const Datastore = require('nedb')
 const app = express()
 app.listen(80, () => console.log('listening on port'))
-app.use(express.static('./'))
+app.use('/Home', express.static('./Feild'))
+app.use('/', express.static('./Menu'))
 app.use(express.json())
 const database = new Datastore('database.db')
 database.loadDatabase()
 
 app.get('/Comments', (req, res) => {
-  database.find({}, (err, data) => {
-    res.json(data)
+  database.find({}).sort({postedAt: -1}).exec((err, data)=>{
+    res.json(data.reverse())
+
   })
 })
 app.post('/Post', (req, res) => {
-  console.log(req)
   database.insert({
     name: 'Bruno',
-    comment: req.body.content
+    comment: req.body.content,
+    postedAt: new Date()
   })
   res.json({status: 'success'})
 })
