0f54ce0057a32e5fe945627d9303341adbf73939
MrPastaPog <bg7sette@gmail.com>
2023-11-27 22:33:58 +1100

    Added Time and date

--- Feild/client.js
+++ Feild/client.js
@@ -1,11 +1,22 @@
+
 $(function() {
+  // if ('scrollRestoration' in window.history) {
+  //   window.history.scrollRestoration = 'manual'
+  // }indow.sc
+  window.scrollTo(0, document.body.scrollHeight || document.documentElement.scrollHeight);
+  let username = JSON.parse(sessionStorage.getItem('Username'))
+  console.log(username)
   fetch('http://localhost/Comments')
   .then(res => res.json()).then(data => {
     console.log(data)
     data.reverse()
     for (d in data) {
       console.log(d)
-      $('#center').prepend(`<div id="comment">${data[d].name}<p>${data[d].comment}</p></div>`)
+      $('#center').prepend(`<div id="comment">
+      ${data[d].name} ${data[d].hours}:${data[d].min} ${data[d].date}/${data[d].month}
+      <p>${data[d].comment}</p>
+
+      </div>`)
     
     }
       
@@ -21,7 +32,7 @@ $(function() {
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({"content": $('textarea').val()})
+      body: JSON.stringify({"content": $('textarea').val(), "username": username})
     }
     fetch('http://localhost/Post', options)
     .then(res => res.json()).then(data => {
--- server.js
+++ server.js
@@ -9,16 +9,20 @@ const database = new Datastore('database.db')
 database.loadDatabase()
 
 app.get('/Comments', (req, res) => {
-  database.find({}).sort({postedAt: -1}).exec((err, data)=>{
-    res.json(data.reverse())
+  database.find({}).sort({postedAt: 1}).exec((err, data)=>{
+    res.json(data)
 
   })
 })
 app.post('/Post', (req, res) => {
   database.insert({
-    name: 'Bruno',
+    name: req.body.username,
     comment: req.body.content,
-    postedAt: new Date()
+    postedAt: new Date(),
+    hours: new Date().getHours(),
+    min: new Date().getMinutes(),
+    month: new Date().getMonth() + 1,
+    date: new Date().getDate()
   })
   res.json({status: 'success'})
 })
